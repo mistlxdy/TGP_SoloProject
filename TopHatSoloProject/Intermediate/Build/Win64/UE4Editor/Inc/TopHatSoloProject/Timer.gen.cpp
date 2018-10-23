5e3a8ccdd6a5107bfbb06dad26320fd3c1f46799
Rhiannon POWELL <p018236h@student.staffs.ac.uk>
2018-10-23 20:50:28 +0100

    nothing changed

--- TopHatSoloProject/Intermediate/Build/Win64/UE4Editor/Inc/TopHatSoloProject/Timer.gen.cpp
+++ TopHatSoloProject/Intermediate/Build/Win64/UE4Editor/Inc/TopHatSoloProject/Timer.gen.cpp
@@ -17,8 +17,6 @@ void EmptyLinkFunctionForGeneratedCodeTimer() {}
 	TOPHATSOLOPROJECT_API UClass* Z_Construct_UClass_ATimer();
 	ENGINE_API UClass* Z_Construct_UClass_AActor();
 	UPackage* Z_Construct_UPackage__Script_TopHatSoloProject();
-	UMG_API UClass* Z_Construct_UClass_UUserWidget_NoRegister();
-	COREUOBJECT_API UClass* Z_Construct_UClass_UClass();
 // End Cross Module References
 	void ATimer::StaticRegisterNativesATimer()
 	{
@@ -33,15 +31,6 @@ void EmptyLinkFunctionForGeneratedCodeTimer() {}
 #if WITH_METADATA
 		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
 #endif
-#if WITH_METADATA
-		static const UE4CodeGen_Private::FMetaDataPairParam NewProp_FailedGame_MetaData[];
-#endif
-		static const UE4CodeGen_Private::FObjectPropertyParams NewProp_FailedGame;
-#if WITH_METADATA
-		static const UE4CodeGen_Private::FMetaDataPairParam NewProp_widgetTemplate_MetaData[];
-#endif
-		static const UE4CodeGen_Private::FClassPropertyParams NewProp_widgetTemplate;
-		static const UE4CodeGen_Private::FPropertyParamsBase* const PropPointers[];
 		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
 		static const UE4CodeGen_Private::FClassParams ClassParams;
 	};
@@ -56,25 +45,6 @@ void EmptyLinkFunctionForGeneratedCodeTimer() {}
 		{ "ToolTip", "#include \"Runtime/UMG/Public/Blueprint/UserWidget.h\"" },
 	};
 #endif
-#if WITH_METADATA
-	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_ATimer_Statics::NewProp_FailedGame_MetaData[] = {
-		{ "EditInline", "true" },
-		{ "ModuleRelativePath", "Public/Timer.h" },
-	};
-#endif
-	const UE4CodeGen_Private::FObjectPropertyParams Z_Construct_UClass_ATimer_Statics::NewProp_FailedGame = { UE4CodeGen_Private::EPropertyClass::Object, "FailedGame", RF_Public|RF_Transient|RF_MarkAsNative, (EPropertyFlags)0x0010000000080008, 1, nullptr, STRUCT_OFFSET(ATimer, FailedGame), Z_Construct_UClass_UUserWidget_NoRegister, METADATA_PARAMS(Z_Construct_UClass_ATimer_Statics::NewProp_FailedGame_MetaData, ARRAY_COUNT(Z_Construct_UClass_ATimer_Statics::NewProp_FailedGame_MetaData)) };
-#if WITH_METADATA
-	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_ATimer_Statics::NewProp_widgetTemplate_MetaData[] = {
-		{ "Category", "Timer" },
-		{ "ModuleRelativePath", "Public/Timer.h" },
-		{ "ToolTip", "Work pls i beg you unreal" },
-	};
-#endif
-	const UE4CodeGen_Private::FClassPropertyParams Z_Construct_UClass_ATimer_Statics::NewProp_widgetTemplate = { UE4CodeGen_Private::EPropertyClass::Class, "widgetTemplate", RF_Public|RF_Transient|RF_MarkAsNative, (EPropertyFlags)0x0014000000010001, 1, nullptr, STRUCT_OFFSET(ATimer, widgetTemplate), Z_Construct_UClass_UUserWidget_NoRegister, Z_Construct_UClass_UClass, METADATA_PARAMS(Z_Construct_UClass_ATimer_Statics::NewProp_widgetTemplate_MetaData, ARRAY_COUNT(Z_Construct_UClass_ATimer_Statics::NewProp_widgetTemplate_MetaData)) };
-	const UE4CodeGen_Private::FPropertyParamsBase* const Z_Construct_UClass_ATimer_Statics::PropPointers[] = {
-		(const UE4CodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_ATimer_Statics::NewProp_FailedGame,
-		(const UE4CodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_ATimer_Statics::NewProp_widgetTemplate,
-	};
 	const FCppClassTypeInfoStatic Z_Construct_UClass_ATimer_Statics::StaticCppClassTypeInfo = {
 		TCppClassTypeTraits<ATimer>::IsAbstract,
 	};
@@ -83,7 +53,7 @@ void EmptyLinkFunctionForGeneratedCodeTimer() {}
 		DependentSingletons, ARRAY_COUNT(DependentSingletons),
 		0x009000A0u,
 		nullptr, 0,
-		Z_Construct_UClass_ATimer_Statics::PropPointers, ARRAY_COUNT(Z_Construct_UClass_ATimer_Statics::PropPointers),
+		nullptr, 0,
 		nullptr,
 		&StaticCppClassTypeInfo,
 		nullptr, 0,
@@ -98,7 +68,7 @@ void EmptyLinkFunctionForGeneratedCodeTimer() {}
 		}
 		return OuterClass;
 	}
-	IMPLEMENT_CLASS(ATimer, 4119098787);
+	IMPLEMENT_CLASS(ATimer, 1424465539);
 	static FCompiledInDefer Z_CompiledInDefer_UClass_ATimer(Z_Construct_UClass_ATimer, &ATimer::StaticClass, TEXT("/Script/TopHatSoloProject"), TEXT("ATimer"), false, nullptr, nullptr, nullptr);
 	DEFINE_VTABLE_PTR_HELPER_CTOR(ATimer);
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- TopHatSoloProject/Intermediate/Build/Win64/UE4Editor/Inc/TopHatSoloProject/TopHatSoloProject.init.gen.cpp
+++ TopHatSoloProject/Intermediate/Build/Win64/UE4Editor/Inc/TopHatSoloProject/TopHatSoloProject.init.gen.cpp
@@ -19,7 +19,7 @@ void EmptyLinkFunctionForGeneratedCodeTopHatSoloProject_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/TopHatSoloProject",
 				PKG_CompiledIn | 0x00000000,
-				0xEA46F8B4,
+				0x8A187840,
 				0xFB7F3EC0,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
